29002a5c1bd25ddaec8d3fe442a018ca051067c5
tamrizki <rizkitam77@gmail.com>
2019-11-22 21:17:18 +0700

    Membuat tampilan app dan inisialisasi objek

--- app/src/main/java/tam/ttr/login/MainActivity.java
+++ app/src/main/java/tam/ttr/login/MainActivity.java
@@ -3,12 +3,32 @@ package tam.ttr.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 
 public class MainActivity extends AppCompatActivity {
+// TODO [3] Inisialisasi komponen yang digunakan pada xml dan google client
+    private TextView tv_name, tv_email;
+    private LinearLayout linearLayout;
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions gso;
+    private GoogleSignInAccount account;
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
